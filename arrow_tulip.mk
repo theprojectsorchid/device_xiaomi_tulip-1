54b37927ec0498768977f8cfceed0b4a116e3490
the projects <84352313+theorchidproject@users.noreply.github.com>
2023-03-08 15:07:31 +0000

    Update arrow_tulip.mk

--- arrow_tulip.mk
+++ arrow_tulip.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Evolution-X stuff
-$(call inherit-product, vendor/evolution/config/common.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Inherit from tulip device
 $(call inherit-product, device/xiaomi/tulip/device.mk)
